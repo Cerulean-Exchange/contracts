dcaaa7fc715bccfa9bc6f55ed8805e13287f544b
GabrielReFintech <gabriel.re@actionfintech.com>
2024-02-27 09:51:14 -0300

    Feat: Addresses TWCORE & USDT for testnet

--- Old/scripts/constants/testnet-config.ts
+++ Old/scripts/constants/testnet-config.ts
@@ -1,8 +1,8 @@
 import {ethers} from "ethers";
 const TEAM_MULTISIG = "0xB123E01098E022755A86C660d18586d55d00d01c";
 const TEAM_EOA = "0x0E70F0eE28c15C3215Ca220Ef3048e3Bc46651DF";
-const TCORE = "";
-const USDT = "";
+const TCORE = "0xbB7855fA0Ad297EC6e4aa1d4BE30f148447eD68c";
+const USDT = "0x3786495f5d8a83b7bacd78e2a0c61ca20722cce3";
 const testnetArgs = {
     TCORE: TCORE,
     USDT: USDT,
