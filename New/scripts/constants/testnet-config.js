389a77086bd7598147b5d3f7832d9ad50863ea52
GabrielReFintech <gabriel.re@actionfintech.com>
2024-03-02 12:03:16 -0300

    Muevo configs a folder nuevo

---
+++ New/scripts/constants/testnet-config.js
@@ -0,0 +1,22 @@
+const TEAM_MULTISIG = "0xB123E01098E022755A86C660d18586d55d00d01c";
+const TEAM_EOA = "0x0E70F0eE28c15C3215Ca220Ef3048e3Bc46651DF";
+const TWCORE = "0xbB7855fA0Ad297EC6e4aa1d4BE30f148447eD68c";
+const USDT = "0x3786495f5d8a83b7bacd78e2a0c61ca20722cce3";
+const testnetArgs = {
+    TWCORE: TWCORE,
+    USDT: USDT,
+    teamEOA: TEAM_EOA,
+    teamTreasure: '0xB123E01098E022755A86C660d18586d55d00d01c',
+    teamMultisig: TEAM_MULTISIG,
+    emergencyCouncil: TEAM_EOA,
+    merkleRoot: "",
+    tokenWhitelist: [],
+    partnerAddrs: [
+
+    ],
+    partnerAmts: [
+
+    ],
+};
+
+export default testnetArgs;
