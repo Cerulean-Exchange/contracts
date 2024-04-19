d6572ee0f8d739e316a72fd70e711d25923b2af3
GabrielReFintech <gabriel.re@actionfintech.com>
2024-04-19 15:30:48 -0300

    Refactor: USDC 6 decimals

--- New/contracts/USDC.sol
+++ New/contracts/USDC.sol
@@ -16,4 +16,8 @@ contract USDC is ERC20, Ownable {
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6; 
+    }
 }
